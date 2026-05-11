1ab3ea3ab20e4908119471ea2bd3855706f527f1
Babafakruddin Darubhaigari <babadarubhaigari@gmail.com>
2026-05-12 00:30:27 +0530

    improved version of add admissions page with models and post request

--- newAdmin/views.py
+++ newAdmin/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
     
 from newAdmin.models import Student
 
@@ -8,9 +8,31 @@ def homepage(request):
     return render(request, 'index.html')
 
 def addAdmission(request):
-    # Fetch all student records from the database
-    students = Student.objects.all()  
-    values = {'allStudents': students}
+
+    # SAVE DATA
+    if request.method == "POST":
+
+        studentName = request.POST.get('name')
+        studentAge = request.POST.get('age')
+        studentGrade = request.POST.get('grade')
+
+        # Store into database
+        Student.objects.create(
+            name = studentName,
+            age = studentAge,
+            grade = studentGrade
+        )
+
+        return redirect('/Ad/newAdm/')
+
+
+    # FETCH DATA
+    students = Student.objects.all()
+
+    values = {
+        'allStudents': students
+    }
+
     return render(request, 'admissions/add-admission.html', values)
 
 def admissionReport(request):
