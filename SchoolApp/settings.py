4e28c389aba858edebe4a7dd0e7844691932bd0a
Babafakruddin Darubhaigari <babadarubhaigari@gmail.com>
2026-05-14 23:43:28 +0530

    django sql connectivity

--- SchoolApp/settings.py
+++ SchoolApp/settings.py
@@ -76,8 +76,10 @@ WSGI_APPLICATION = 'SchoolApp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'school_db',
+        'USER': 'school_app_user',
+        'PASSWORD': '#BVsS@0001',
     }
 }
 
